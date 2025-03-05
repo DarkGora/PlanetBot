f83ef7e2912452d6b74bb2c9a5c5c325a4feb12d
andre <andrey.evushvggtjo@gmail.com>
2025-03-05 19:09:33 +0300

    Flot2

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -35,8 +35,16 @@ public class Bot extends TelegramLongPollingBot {
         listQuestion.add(new Question("Какие элементы может содержать класс", List.of("Поля", "Конструкоры", "Методы", "Интерфейсы", "Все вышеперечислонные"), 4));
         listQuestion.add(new Question("Что означает этот метасимвол регулярных выражений -$ ", List.of("Начало строки", "Конец строки", "Начало слова", "Конец ввода"), 1));
         listQuestion.add(new Question("Что озн  ачает этот метасимвол регулярных выражений -\s ", List.of("Цифровой символ", "Не цифровой символ", "символ пробела", "бкувенно-цифровой символ", "Все вышеперечислонные"), 2));
-//hnk;
-    }
+        listQuestion.add(new Question ("Какой из следующих типов данных является примитивным в Java?", List.of("String","Integer","int","ArrayList"), 2));
+        listQuestion.add(new Question ("Какой из следующих операторов используется для сравнения двух значений в Java?", List.of("=","==","===","!="),1));
+        listQuestion.add(new Question ("Какой метод используется для запуска программы в Java?", List.of("main()","start()","run()","startJava()"),0));
+        listQuestion.add(new Question ("Как останосить case?",List.of("break","stop","stopline","short"),3));
+        listQuestion.add(new Question ("Какой из следующих интерфейсов используется для работы с коллекциями в Java?", List.of("List","Map","Eilast","Collection"),1));
+        listQuestion.add(new Question ("Какой модификатор доступа делает член класса доступным только внутри этого класса?", List.of("public","String","private","ModerPriv"),0));
+        listQuestion.add(new Question ("Что такое исключение в Java?",List.of("Ошибка компиляции","Исключение обьекта путем команд","Doms","Где?"),3));
+        listQuestion.add(new Question ("Какой из следующих классов является частью Java Collections Framework?",List.of("HashMap","Scanner","Framework","Collection"),1));
+        listQuestion.add(new Question ("Какой оператор используется для создания нового объекта в Java?",List.of("new","object","ineselert","int"),1));
+        listQuestion.add(new Question ("Какой из следующих методов позволяет получить длину массива в Java?",List.of("length()","size()","getlength()","length"),0));    }
 
     @Override
     public String getBotUsername() {
